2ff757717620dadc96e36f821774f0507a90b8c5
lemokh <byndasma@gmail.com>
2020-08-14 23:11:13 +0400

    folders to contain App, Navbar, Video, AboutUs files

---
+++ src/AboutUs/AboutUs.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../Navbar/Navbar";
+
+function AboutUs() {
+  return (
+    <div>
+      <Navbar />
+      <h1>About us</h1>
+      <div></div>
+    </div>
+  );
+}
+
+export default AboutUs;

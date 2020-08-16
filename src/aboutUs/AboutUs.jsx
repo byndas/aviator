c37c0fe37817988b4841bbf87b9983f6c8a2e6d2
lemokh <byndasma@gmail.com>
2020-08-16 12:57:26 +0400

    cleaner file system

---
+++ src/aboutUs/AboutUs.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../navbar/Navbar";
+
+function AboutUs() {
+  return (
+    <div>
+      <Navbar />
+      <h1>About us</h1>
+      <div></div>
+    </div>
+  );
+}
+
+export default AboutUs;

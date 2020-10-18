70f252eecb4fdc97d214ce54bda5c49e3badd21b
lemokh <byndasma@gmail.com>
2020-10-18 13:29:53 +0400

    editForm all works but must upload image

--- src/redux/news/news.reducer.js
+++ src/redux/news/news.reducer.js
@@ -1,12 +1,5 @@
 import { newsActionTypes } from "./news.types";
 
-// const INITIAL_STATE = {
-//   name: null,
-//   src: null,
-//   text: null,
-//   title: null
-// };
-
 export const newsReducer = (state = {}, action) => {
   switch (action.type) {
     case newsActionTypes.FIREBASE_NEWS:
@@ -18,14 +11,15 @@ export const newsReducer = (state = {}, action) => {
       return Object.assign({}, state);
 
     case newsActionTypes.EDIT_NEWS:
-      console.log("REDUX state", state);
+      console.log("REDUX STATE", state);
       console.log("action.id", action.id);
       console.log("action.payload", action.payload);
 
       const updateID = action.id;
       const updatePost = action.payload;
       // UPDATES POST ID DATA OBJECT
-      state[updateID] = state[updatePost];
+      state[updateID] = updatePost;
+
       console.log("REDUX news STATE", state);
       return Object.assign({}, state);
 
